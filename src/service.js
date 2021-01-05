2454f3b3b5ab3a579451e40548b6f3b0ffa91d9d
sean <xiaoxiaojing@coding.net>
2021-01-05 12:38:12 +0800

    deploy

--- src/service.js
+++ src/service.js
@@ -1,9 +1,13 @@
 import request from "umi-request";
 
 export const getUseInfo = (code) => {
-  return request.get(`https://test.sxjycszs.com/v1/front/wx/user?code=${code}`);
+  return request.get(
+    `http://101.200.207.61:9988/v1/front/wx/user?code=${code}`
+  );
 };
 
 export const getSignature = (url) => {
-  return request.get(`https://test.sxjycszs.com/v1/front/wx/config?url=${url}`);
+  return request.get(
+    `http://101.200.207.61:9988/v1/front/wx/config?url=${url}`
+  );
 };
